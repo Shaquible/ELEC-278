773c4325fccb205502509fc76a17fc8e3025a9b4
Keegan Kelly <keegkelly2013@gmail.com>
2021-09-13 20:36:59 -0400

    finished first draft

--- Lab 1/Lab01Materials/readnumsv5.c
+++ Lab 1/Lab01Materials/readnumsv5.c
@@ -1,86 +1,60 @@
-// READNUMSV5.C - Lab01 - David Athersych
-//
-// Program to read numbers.
-// Version 5 is a combination of version 3 and version 4 - it uses a linked
-// list but it allows data to be added either at the front or at the back.
-// Note that the data entry method has changed - each number has a preceeding
-// letter indicating whether the number is to be added at the front, or the end
-// or whether input is finished.
-//
-// HISTORY
-// 200726 - DFA - First release
-
-/* --README--------------------------------------------------------------------
-Source code used for teaching purposes in course:
-ELEC278, Queen's University, Kingston, Fall semester 2020.
-This code is provided without warranty of any kind. It is the responsibility
-of the user to determine the correctness and the usefulness of this code for
-any purpose.
-
-Author:  David F. Athersych, P.Eng.
-All rights reserved. This code is intended for students registered in the
-course and the semester listed above.
-
-See LICENCE.MD for restrictions on the use of this code.
----------------------------------------------------------------------------- */
+// READNUMSV6.C - Lab01 - Keegan, Kelly
 
 #include <stdio.h>
 
-
 // Handy manifests
-#define	EXIT_OK		0
-#define	EXIT_ERROR	-1
+#define EXIT_OK 0
+#define EXIT_ERROR -1
 
-
-// Definition for what node in our linked list looks like 
-struct node {
-	struct node	*pnextnode;
-	int			datavalue;
-	};
+// Definition for what node in our linked list looks like
+struct node
+{
+	struct node *pnextnode;
+	int datavalue;
+};
 
 // Global pointers - so they can be used by all functions. One points
 // to the first element in the linked list, the other points to the last
 // element in the linked list.
-struct node	*phead = NULL;
+struct node *phead = NULL;
 struct node *pback = NULL;
-
 
 // Notice that we have extracted a piece of code that was common to both
 // the insert_data_at_front() and insert_data_at_end() routines in the
 // previous program versions. The next function creates a new node and
 // initializes the data and next pointer fields.
 
-struct node *pnew_node (int d)
+struct node *pnew_node(int d)
 // Function creates a new node, and initializes the structure fields. Function
 // returns pointer to the new node if everything worked, or it returns NULL
 // because malloc() was unable to ge memory.
 // Notice we have started taking some coding shortcuts here
 {
 	// Declaration of local node pointer, with initialization
-	struct node	*pn = (struct node *) malloc (sizeof (struct node));
+	struct node *pn = (struct node *)malloc(sizeof(struct node));
 
 	// If we actually got memory, use it
-	if (pn != NULL)	{
+	if (pn != NULL)
+	{
 		// Now fill in structure. Notice the syntax. You can read this as
 		// "assign value in d to datavalue field in structure pointed
 		//  to by pn".
 		pn->datavalue = d;
-		pn->pnextnode = NULL;	// guarantees that it has unmistakeable value
-		}
+		pn->pnextnode = NULL; // guarantees that it has unmistakeable value
+	}
 	// Get here with pn pointing to a filled-in structure, or with pn equal
 	// to NULL because malloc() failed. In either case, return pn.
 	return pn;
-}//pnew_node()
-	
+} //pnew_node()
 
-
-int insert_data_at_front (int d)
+int insert_data_at_front(int d)
 // Insert a new item of data at the front of the linked list.
 {
-	struct node	*pn = pnew_node (d);
+	struct node *pn = pnew_node(d);
 
 	// Make sure node creation worked
-	if (pn == NULL)	return -1;
+	if (pn == NULL)
+		return -1;
 
 	// New node goes at the head of the list. The current head of the list
 	// is pointed to by phead, so we make the new node point to the place
@@ -91,110 +65,174 @@ int insert_data_at_front (int d)
 	phead = pn;
 	// check if this was the very first node added - if so, we have to update
 	// the end pointer as well
-	if (pback == NULL)	pback = pn;
+	if (pback == NULL)
+		pback = pn;
 	return 0;
-}//insert_data_at_front()
+} //insert_data_at_front()
 
-
-int insert_data_at_end (int d)
+int insert_data_at_end(int d)
 // Insert new item of data at end of linked list.
 {
-	struct node	*pn = pnew_node (d);
+	struct node *pn = pnew_node(d);
 
 	// make sure node creation worked
-	if (pn == NULL)	return -1;
+	if (pn == NULL)
+		return -1;
 
 	// New node goes at back of list. If the list is currently empty,
 	// then both the head and back pointers need to be updated.
-	if (pback == NULL)	{
+	if (pback == NULL)
+	{
 		pback = pn;
 		phead = pn;
-	} else	{
+	}
+	else
+	{
 		// there are already nodes in the list. Add the new one after
 		// the existing last one.
 		pback->pnextnode = pn;
 		// make the back pointer point to the new one
 		pback = pn;
-		}
+	}
 	return 0;
-}//insert_data_at_end()
+} //insert_data_at_end()
 
+//function to delete a node in the linked list containing the data value num
+//removes the all occurrences of the number from the list
+void delete_node(int num)
+{
+	//intializing the curent pointer and previous pointer
+	struct node *current_pointer = phead;
+	struct node *prev_pointer = NULL;
+	//looping through the linked list to find the number and delete it
+	while (current_pointer != NULL)
+	{
+		//checks if the current nodes number is the number we want to delete
+		if (current_pointer->datavalue == num)
+		{
+			//checks if the current pointer is the head pointer
+			if (phead->datavalue == num)
+			{
+				//updates the head pointer
+				phead = current_pointer->pnextnode;
+				//frees the pointer
+				free(current_pointer);
+				//re-sets the value of the current pointer so the list can continue to be iterated through to check for multiple occurrences
+				current_pointer = phead;
+				continue;
+			}
+			//checks if the current pointer is the back pointer
+			if (current_pointer->pnextnode == NULL)
+			{
+				//updates the back pointer
+				pback = prev_pointer;
+				//sets back pointers next node back to null
+				pback->pnextnode = NULL;
+				//frees pointer
+				free(current_pointer);
+				break;
+			}
+			//removes the current node from the list
+			prev_pointer->pnextnode = current_pointer->pnextnode;
+			//frees current pointer
+			free(current_pointer);
+			//re-sets the value of the current pointer so the list can continue to be iterated through to check for multiple occurrences
+			current_pointer = prev_pointer->pnextnode;
+			continue;
+		}
+		//updates previous and current pointer for next loop iteration if the desired node was not found and deleted
+		prev_pointer = current_pointer;
+		current_pointer = current_pointer->pnextnode;
+	}
+}
 
-void print_linked_list (void)
+void print_linked_list(void)
 // Routine prints data contents of linked list
 {
-	struct node	*ptemp;		// temporary pointer used to traverse list
-	int			count;		// count how many printed on one line
-	if (phead == NULL)	{
-		printf ("<EMPTY LIST>\n");
+	struct node *ptemp; // temporary pointer used to traverse list
+	int count;			// count how many printed on one line
+	if (phead == NULL)
+	{
+		printf("<EMPTY LIST>\n");
 		return;
-		}
+	}
 	// List has some real data to be printed. We will point ptemp at
-	// the first node in the list, print the data, and if there is 
+	// the first node in the list, print the data, and if there is
 	// more list, move ptemp to point to the next node. We'll keep
 	// doing this until there are no more nodes to look at.
 	ptemp = phead;
-	count = 0;				// 0 on line so far
-	while (ptemp != NULL)	{
-		printf ("%4d", ptemp->datavalue);
-		if (ptemp->pnextnode != NULL)	{
-			printf ("->");
-			count++;			// one more on the line
+	count = 0; // 0 on line so far
+	while (ptemp != NULL)
+	{
+		printf("%i", ptemp->datavalue);
+		if (ptemp->pnextnode != NULL)
+		{
+			printf("->");
+			count++; // one more on the line
 			// check if getting close to right edge - if so, move to start of
 			// next line and reset count.
-			if (count > 12)	{
-				putchar ('\n');
+			if (count > 12)
+			{
+				putchar('\n');
 				count = 0;
-				}
-		} else	{
-			putchar ('\n');
 			}
-		ptemp = ptemp->pnextnode;
 		}
+		else
+		{
+			putchar('\n');
+		}
+		ptemp = ptemp->pnextnode;
+	}
 	// all done
 	return;
 } // print_linked_list()
-	
 
-void main (void)
+void main(void)
 {
-	char	ch;
-	int		d;
-	int		going;
+	char ch;
+	int d;
+	int going;
 
-	printf ("On each line, enter letter F or B, followed by a space and\n");
-	printf ("a number. F means the number is placed at the front of the\n");
-	printf ("list and B means the number is placed at the back\n");
-	printf ("To end your input, enter letter Q and any number.\n");
+	printf("On each line, enter letter F or B, followed by a space and\n");
+	printf("a number. F means the number is placed at the front of the\n");
+	printf("list and B means the number is placed at the back\n");
+	printf("To end your input, enter letter Q and any number.\n");
 
 	going = 1;
-	while (going)	{
+	while (going)
+	{
 		// very specific format !!
-		scanf ("%c", &ch);			// get the letter in the first position
-		scanf ("%d", &d);			// get the number after that
-		while (getchar() != '\n');	// skip all up to and including newline
-		printf ("Received %c %d\n", ch, d);
-		switch (ch)	{
+		scanf("%c", &ch); // get the letter in the first position
+		scanf("%d", &d);  // get the number after that
+		while (getchar() != '\n')
+			; // skip all up to and including newline
+		printf("Received %c %d\n", ch, d);
+		switch (ch)
+		{
 		case 'Q':
 		case 'q':
-			going = 0;		// Q or q means end of input
+			going = 0; // Q or q means end of input
 			break;
 		case 'F':
 		case 'f':
-			insert_data_at_front (d);
+			insert_data_at_front(d);
 			break;
 		case 'B':
 		case 'b':
-			insert_data_at_end (d);
+			insert_data_at_end(d);
+			break;
+		case 'D':
+		case 'd':
+			delete_node(d);
 			break;
 		default:
-			fprintf (stderr, "ERROR: Invalid command --%c--\n", ch);
+			fprintf(stderr, "ERROR: Invalid command --%c--\n", ch);
 			break;
-			}//endswitch
-		}//endwhile
+		} //endswitch
+	}	  //endwhile
 
 	// User has entered all they wanted to
-	print_linked_list ();
+	print_linked_list();
 
-	printf ("====END====\n");
-}//main()
+	printf("====END====\n");
+} //main()
