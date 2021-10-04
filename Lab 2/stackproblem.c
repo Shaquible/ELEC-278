a5091f5f312d87d75b7508d53c3bba9a1abacecb
Keegan Kelly <keegkelly2013@gmail.com>
2021-10-04 18:32:29 -0400

    fjklsdhfjklsad

--- Lab 2/stackproblem.c
+++ Lab 2/stackproblem.c
@@ -1,29 +1,37 @@
 //stackproblem.c - Lab 02 - Keegan, Kelly
 #include <stdio.h>
+//initializing the stack
 char stack[80];
 int top = -1;
+
 int push(char data);
 int main(void)
 {
     int go = 1;
-
+    //while loop that continues to scan for user input
     while (1)
     {
         char temp;
         scanf("%c", &temp);
+        //loop is broken on newline
         if (temp == '\n')
             break;
+        //pushes data to the stack
         if (push(temp) == 0)
-            return 0;
+            //exits if the stack is full
+            break;
     }
+    //prints each element of the stack starting from the top down
     for (int i = top; i >= 0; i--)
     {
         printf("%c", stack[i]);
     }
+    //prints a new line unless the stack is empty
     if (top >= 0)
         printf("\n");
 }
 
+//function that pushes data to the top of the stack and returns 0 if the stack is full
 int push(char data)
 {
     if (top == 79)
